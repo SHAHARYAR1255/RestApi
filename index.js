c3034251aa28753c60e76796c32030f1f5b8267a
= <shaharyar.malik2000@gmail.com>
2020-09-04 06:51:38 +0500

    sec but first

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const app = express();
 let port = process.env.PORT || 3000 ;
-const importData = require('./db.json');
+const importDishes = require('./dishes.json');
+const importPromo = require('./promo.json');
+const importLeader = require('./leader.json');
+const importFeedback = require('./db.json');
+const importComment = require('./comment.json');
 
 app.get("/", (req, res) =>{
     res.send('Hello World');
-    res.send(importData);
 });
-app.get("/players", (req, res) =>{
-    res.send(importData);
+app.get("/dishes", (req, res) =>{
+    res.send(importDishes);
+});
+app.get("/promotions", (req, res) =>{
+    res.send(importPromo);
+});
+app.get("/leaders", (req, res) =>{
+    res.send(importLeader);
+});
+app.get("/feedbacks", (req, res) =>{
+    res.send(importFeedback);
+});
+app.get("/comments", (req, res) =>{
+    res.send(importComment);
 });
 
 app.listen(port , ()=>{
-    console.log('its listennign on piet')
+    console.log('its listennign....')
 })
